788086b325bfdf4d818ea8a54cd08aa439af79a2
root <root@debian>
2018-11-18 16:52:14 +0200

    First Push

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
 <html>
 <body>
 <center>
-<h3>Welcome To Open Shift Test Applicatio</h3>
+<h3>Welcome To Open Shift Test Application</h3>
 <h4><?php $date = date("Y-m-d H:i:s");  echo $date;  ?></h4>
+<p>First Deployment Application</p>
 </center>
 </body>
 </html>
