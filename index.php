9e9db2b424a75ecbe338d054dca982e386f94b4c
root <root@debian>
2018-11-18 17:27:21 +0200

    save

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <html>
-<body style="ibackground-color:#f1f1f1">
+<body style="background-color:#E5E5E5">
 <center>
 <h3>Welcome To Open Shift Test Application</h3>
 <h4><?php $date = date("Y-m-d H:i:s");  echo $date;  ?></h4>
