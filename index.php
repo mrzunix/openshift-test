5c00370a18bebc9d7570c550062b2c1e2262636e
root <root@debian>
2018-11-23 20:39:54 +0200

    save

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 <h3>Welcome To Open Shift Test Application</h3>
 <h4><?php $date = date("Y-m-d H:i:s");  echo $date;  ?></h4>
 <p>That Is My First Auto Deployment Application</p>
+<h3>Abdelrahman Essam </h3>
 </center>
 </body>
 </html>
