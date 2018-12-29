daebd6aa42b57be5d07f1074eaa6c6f3fc9083d9
Ahmed <mr.zunix@gmailc.om>
2018-12-29 13:16:01 +0200

    save

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <body style="background-color:#E5E5E5">
 <center>
 <h3>Welcome Jenkins Auto Deployment Application Application</h3>
+<h3>Welcome Jenkins Auto Deployment Application Application</h3>
 <h4><?php $date = date("Y-m-d H:i:s");  echo $date;  ?></h4>
 <p>That Is My First Auto Deployment Application</p>
 </center>
