80588b1e8438afa17e8e3b86ac22d04bd45938e5
Ahmed Zaki <mr.zunix@gmail.com>
2018-12-29 12:30:58 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <html>
 <body style="background-color:#E5E5E5">
 <center>
-<h3>Welcome To Open Shift Test Application</h3>
+<h3>Welcome Jenkins Auto Deployment Application Application</h3>
 <h4><?php $date = date("Y-m-d H:i:s");  echo $date;  ?></h4>
 <p>That Is My First Auto Deployment Application</p>
 <h3>Abdelrahman Essam </h3>
